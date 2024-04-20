cf2cf72c8ffb70e08a80ab0527dd7b45843d0fd6
Malo LM <58141930+MaloLM@users.noreply.github.com>
2024-04-20 20:44:56 +0200

    implements setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
-# Setup script for the project
+from setuptools import setup
+
+def read_requirements():
+    with open('requirements.txt', 'r') as req:
+        return req.read().splitlines()
+
+setup(
+    name="educationnal-neural-network",
+    version="1.0",
+    description="Python implementation of an artificial neural network aimed at deeply mastering the underlying concepts that drive neural networks. This project is inspired by Andrej Karpathy and Russ Salakhutdinov.",
+    author="Malo Le Mestre",
+    author_email="malo.lm@icloud.com",
+    packages=["educationnal-neural-network"],
+    install_requires=read_requirements(),
+)
